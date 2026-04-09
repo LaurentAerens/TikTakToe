c364630e44f83adffe8ccc0ae4df1d87b27f178f
Laurent Aerens <Laurent.Aerens@codit.eu>
2026-04-09 16:29:10 +0200

    feat: Enhance HalfDepthBoardEvaluator with additional scoring logic and tests for overlapping threats

--- src/backend/TikTakToe.Tests/engines/HalfDepthEngineTest.cs
+++ src/backend/TikTakToe.Tests/engines/HalfDepthEngineTest.cs
@@ -85,6 +85,38 @@ public class HalfDepthBoardEvaluatorTest
     }
 
     [Fact]
+    public void Eval_OverlappingThreatsOnSameSquare_CountsOnce()
+    {
+        var evaluator = new HalfDepthBoardEvaluator();
+        var board = new int[3, 3]
+        {
+            { 0, 1, 0 },
+            { 1, 0, 1 },
+            { 0, 1, 0 }
+        };
+
+        var score = evaluator.Evaluate(board);
+
+        Assert.Equal(500, score);
+    }
+
+    [Fact]
+    public void Eval_TwoThreatsOnDifferentSquares_ScoresAs1000()
+    {
+        var evaluator = new HalfDepthBoardEvaluator();
+        var board = new int[3, 3]
+        {
+            { 1, 1, 0 },
+            { 0, 0, 0 },
+            { 1, 1, 0 }
+        };
+
+        var score = evaluator.Evaluate(board);
+
+        Assert.Equal(1000, score);
+    }
+
+    [Fact]
     public void Eval_NonTerminalBoard_ReturnsHeuristicInRange()
     {
         var evaluator = new HalfDepthBoardEvaluator();
